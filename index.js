7b693b3e2ae34dda6b0cae5e3f5152d707074853
Swarnim <rawatswarnim25@gmail.com>
2020-07-04 22:58:29 +0530

    updated menu buttons and some other fixes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const $ = require("jquery");
 const electron = require("electron");
 const fs = require("fs");
-
 const dialog = require("electron").remote.dialog;
 $(document).ready(
     function () {
